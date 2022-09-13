eeae67ad687c797298d4e8d4a1cecba22a2947f2
azrael18102001 <105088428+azrael18102001@users.noreply.github.com>
2022-09-13 12:35:44 +0530

    devesh

---
+++ my_dost/message.py
@@ -0,0 +1,27 @@
+
+# import string
+
+
+from typing import Union,List
+# from my_dost.helpers import dostify
+
+def msg_box_info(msg_for_user:str):
+    """Display a message box with an 'OK' button.
+
+    Args:
+        msg_for_user (str): The message to display to the user.
+
+    Examples:
+        >>> msg_box_info('This is a demo message.')
+
+
+    """
+    import tkinter as tk
+    from tkinter import messagebox
+    root = tk.Tk()
+    root.attributes('-topmost', True)
+    root.withdraw()
+    tk.messagebox.showinfo('PyBOTs', msg_for_user, parent=root)
+    root.destroy()
+
+msg_box_info(['This is a demo message.\n','sad'])
